34bfb980e057cbccc467282459d728434c8c3e33
Oliver Herrmann <herrmann@id-on.de>
2021-02-04 12:03:01 +0100

    Add Relations DAO

--- app/Tasks/Relations.hs
+++ app/Tasks/Relations.hs
@@ -1,45 +1,23 @@
 module Tasks.Relations (runTask) where
 
-import Prelude hiding ((+), (-), (*))
-import Data.List (intercalate)
-import Text.Parsec ((<|>))
-import qualified Text.Parsec as P
-import Data.Set (Set)
+import qualified Data.Set as S
+import Autotool.DAO (toValue)
+import Autotool.DAO.Set (Set)
+import Autotool.DAO.Relation (RelOp)
+import Autotool.DAO.Identifier (Identifier)
 import Autotool.Data.LazyTree (isOp0, Op(..), showTree, eval, trees)
-import Autotool.Parser.Relation ( parseRelation )
-import Autotool.Data.RelOp ((+), (&), (-), (*))
 import Autotool.Solver.Relations (solve)
 
 runTask :: String -> String
-runTask input = case parseSetDescription input of
-    (Right d@(RelationDescription ops t)) -> showTree $ solve ops t
-    (Left err) -> show err
+runTask input = showTree $ solve (rops ++ ops) t
+    where
+        desc = read input :: RelationDescription
+        ops = (map toValue $ operators desc) :: [Op (S.Set (Int,Int))]
+        rops = map (\(name,rel) -> Op0 (show name) (toValue rel)) (relations desc) :: [Op (S.Set (Int,Int))]
+        t = (toValue $ target desc) :: S.Set (Int,Int)
 
 data RelationDescription = RelationDescription
-    { ops :: [Op (Set (Integer,Integer))]
-    , target :: Set (Integer,Integer)
-    }
-
-instance Show RelationDescription where
-    show (RelationDescription ops t) =
-        let target = "Target: " ++ show t
-            sets = "Relations: " ++ intercalate ", " (map (\cnst -> show cnst ++ " = " ++ show (eval cnst [])) $ filter isOp0 ops)
-            ops' = "Operators: " ++ intercalate ", " (map show $ filter (not . isOp0) ops)
-        in unlines [ops', sets, target]
-
-parseSetDescription :: String -> Either P.ParseError RelationDescription
-parseSetDescription = P.parse parse ""
-    where
-        parse = let
-            f ops ss = RelationDescription (ss ++ ops)
-            in f <$> parseOPs <*> parseRelations <*> parseTarget
-        parseOPs = (P.string "#OPS" >> P.spaces) *> P.many1 parseOp <* P.spaces
-        parseOp = parseOpCup <|> parseOpDiff <|> parseOpCap <|> parseOpCompose
-        parseOpCup = P.char '+' >> P.spaces >> return (+)
-        parseOpDiff = P.char '-' >> P.spaces >> return (-)
-        parseOpCap = P.char '&' >> P.spaces >> return (&)
-        parseOpCompose = P.string "." >> P.spaces >> return (*)
-        parseTarget = (P.string "#TARGET" >> P.spaces) *> parseRelation <* P.spaces
-        parseRelations = (P.string "#RELATIONS" >> P.spaces) *> P.many1 parseConst <* P.spaces
-        parseConst = Op0 <$> (P.many1 P.alphaNum <* parseEq) <*> (parseRelation <* P.spaces)
-        parseEq = P.spaces >> P.char '=' >> P.spaces
+    { operators :: [RelOp Int]
+    , relations :: [(Identifier, Set (Int, Int))]
+    , target :: Set (Int,Int)
+    } deriving (Show,Read)
---
+++ src/Autotool/DAO/Relation.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Autotool.DAO.Relation (RelOp) where
+
+import Prelude hiding ((+), (-), (&), (*))
+import Data.Set ( Set )
+import qualified Text.ParserCombinators.ReadP as P
+import Text.ParserCombinators.ReadP (ReadP)
+import Control.Applicative (Alternative((<|>)))
+import Data.Functor (($>))
+import Autotool.DAO (DAO(..))
+import Autotool.Readable (Readable(..), spaces)
+import Autotool.Data.RelOp ((+), (-), (&), (*))
+import Autotool.Data.LazyTree (Op)
+
+{- Relation OPERATIONS -}
+
+data RelOp a = OpSum | OpUnion | OpDiff | OpCompose
+
+instance Show (RelOp a) where
+    show OpSum = "+"
+    show OpUnion = "&"
+    show OpDiff = "-"
+    show OpCompose = "."
+
+instance Read (RelOp a) where
+    readsPrec _ = P.readP_to_S readP
+
+instance Readable (RelOp a) where
+    readP = readSum <|> readUnion <|> readDiff <|> readCompose
+        where
+            readSum = (spaces >> P.char '+' >> spaces) $> OpSum
+            readUnion = (spaces >> P.char '&' >> spaces) $> OpUnion
+            readDiff = (spaces >> P.char '-' >> spaces) $> OpDiff
+            readCompose = (spaces >> P.string "." >> spaces) $>  OpCompose
+
+instance (Num a, Ord a) => (DAO (Op (Set (a,a)))) (RelOp a) where
+    toValue OpSum = (+)
+    toValue OpUnion = (&)
+    toValue OpDiff = (-)
+    toValue OpCompose = (*)
